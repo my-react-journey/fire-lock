41bd41383edd912f5cdf1b84b491d1a83d13230d
tharunoptimus <tharunoptimus@outlook.com>
2022-10-27 15:45:55 +0530

    Added AccountCard

---
+++ src/App/AccountCard.js
@@ -0,0 +1,35 @@
+import right from "./right.svg"
+import styles from "./AccountCard.module.css"
+import { useNavigate } from "react-router-dom"
+
+function AccountCard(props) {
+    
+    let navigate = useNavigate()
+    console.log(props)
+
+    let handleClick = () => {
+        let {id} = props
+        navigate(`/account/${id}`)
+    }
+
+	return (
+		<>
+            <div className={styles.cardHolder} onClick={handleClick}>
+                <div className={styles.logoHolder}>
+                    <img src={props.accountLabel} alt={props.label} />
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.content}>
+                        <span className={styles.contentLabel}>{props.label}</span>
+                        <span className={styles.contentEmail}>{props.account}</span>
+                    </div>
+                    <div className={styles.navigatorHolder} onClick={handleClick}>
+                        <img src={right} alt="View" />
+                    </div>
+                </div>
+            </div>
+		</>
+	)
+}
+
+export default AccountCard
