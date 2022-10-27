68f04e93f773eaaf126b986220d26cd9568a88f3
tharunoptimus <tharunoptimus@outlook.com>
2022-10-27 14:08:44 +0530

    Refactor File Organizing

---
+++ src/App/index.js
@@ -0,0 +1,26 @@
+import "./App.css"
+import {get, set} from "idb-keyval"
+
+function App() {
+
+		
+
+	return (
+		<>
+			<header>
+				<h1>Fire Lock</h1>
+
+				<span></span>
+				<span></span>
+				
+			</header>
+
+			<main>
+
+			</main>
+		
+		</>
+	)
+}
+
+export default App
