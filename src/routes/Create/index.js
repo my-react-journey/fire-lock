627967eb5437d55d4fa0ec93d90acb8cf5989847
tharunoptimus <tharunoptimus@outlook.com>
2022-10-27 21:51:06 +0530

    Added QR Code scanner component to `Create`

--- src/routes/Create/index.js
+++ src/routes/Create/index.js
@@ -1,7 +1,43 @@
+import Title from "../../components/Title"
+import { QrReader } from "react-qr-reader"
+import { useState } from "react"
+import styles from "./Create.module.css"
+
 export default function Create() {
+	let [data, setData] = useState("No result")
+
 	return (
 		<>
-			<span>This is the Create page</span>
+			<Title titleName="Add an account" />
+			<div className={styles.container}>
+				<span className={styles.infoSpan}>
+					Your account provider will display a QR Code. Scan it to
+					add.
+				</span>
+				<QrReader
+					constraints={{ facingMode: "environment" }}
+					containerStyle={{
+						width: "65vw",
+						borderRadius: "12px",
+						display: "grid",
+						placeItems: "center",
+					}}
+					videoContainerStyle={{ width: "65vw", borderRadius: "12px", paddingTop: "98%" }}
+					videoStyle={{ width: "85vw", height: "unset", borderRadius: "12px" }}
+					onResult={(result, error) => {
+						if (!!result) {
+							setData(result?.text)
+						}
+
+						if (!!error) {
+							console.info(error)
+						}
+					}}
+					style={{ width: "100%" }}
+				/>
+				<p>{data}</p>
+			</div>
+			<span></span>
 		</>
 	)
 }
