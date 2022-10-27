9418074dd1b39c343d250365017867802c16e7de
tharunoptimus <tharunoptimus@outlook.com>
2022-10-27 15:47:12 +0530

    App component is ready

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,55 @@
-import "./App.css"
-import {get, set} from "idb-keyval"
+import styles from "./App.module.css"
+import menu from "./menu.webp"
+import plus from "./plus.webp"
+import { get, set } from "idb-keyval"
+import { useEffect, useState, useId } from "react"
+import { useNavigate } from "react-router-dom"
+
+
+import AccountCard from "./AccountCard"
 
 function App() {
+    let navigate = useNavigate()
+	let [acccounts, setAccounts] = useState()
 
-		
+	let addHandler = () => navigate("/create")
+	let menuHandler = () => navigate("/menu")
+
+
+
+	useEffect(() => {
+		get("accounts").then((accounts) => {
+			setAccounts(accounts)
+		})
+	}, [])
 
 	return (
 		<>
-			<header>
-				<h1>Fire Lock</h1>
-
-				<span></span>
-				<span></span>
+			<div className={styles.header}>
 				
-			</header>
+				<span className={styles.appName}>Fire Lock</span>
 
-			<main>
+				<span className={styles.actionIconsAdd} onClick={addHandler}>
+					<img src={plus} alt="Add" />
+				</span>
 
-			</main>
-		
+				<span className={styles.actionIconsMenu} onClick={menuHandler}>
+					<img src={menu} alt="Menu" />
+				</span>
+			</div>
+
+			<div className={styles.mainContent}>
+				{acccounts &&
+					acccounts.map((account) => (
+						<AccountCard
+							key={account.id}
+							id={account.id}
+							label={account.label}
+							account={account.account}
+							accountLabel={account.accountLabel}
+						/>
+					))}
+			</div>
 		</>
 	)
 }
