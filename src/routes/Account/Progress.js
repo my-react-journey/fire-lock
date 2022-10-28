adeb3bd33c69aa2748ec494e16e28bfa0ea2b8ad
tharunoptimus <tharunoptimus@outlook.com>
2022-10-29 00:38:52 +0530

    Added `Pie` component for progress circle Code copied from [Dev.to](https://dev.to/jackherizsmith/making-a-progress-circle-in-react-3o65)

---
+++ src/routes/Account/Progress.js
@@ -0,0 +1,53 @@
+const cleanPercentage = (percentage) => {
+	const isNegativeOrNaN = !Number.isFinite(+percentage) || percentage < 0
+	const isTooHigh = percentage > 100
+	return isNegativeOrNaN ? 0 : isTooHigh ? 100 : +percentage
+}
+
+const Circle = ({ colour, percentage }) => {
+	const r = 15
+	const circ = 2 * Math.PI * r
+	const strokePct = ((100 + percentage) * circ) / 100
+	return (
+		<circle
+			r={r}
+			cx={50}
+			cy={50}
+			fill="transparent"
+			stroke={strokePct !== circ ? colour : ""} 
+			strokeWidth={"3px"}
+			strokeDasharray={circ}
+			strokeDashoffset={percentage ? strokePct : 0}
+			transform={"rotate(-90, 60,60)"}
+		></circle>
+	)
+}
+
+const Text = ({ text }) => {
+	return (
+		<text
+			x="50%"
+			y="50%"
+			dominantBaseline="central"
+			textAnchor="middle"
+			fill="#FFF"
+			fontSize={"1.1em"}
+		>
+			{text}
+		</text>
+	)
+}
+
+const Pie = ({ percentage, colour, text }) => {
+	const pct = cleanPercentage(percentage)
+	return (
+		<svg width={40} height={40}>
+			<g style={{ transform: "translate(-30px, -50px)" }}>
+				<Circle colour={colour} percentage={pct} />
+			</g>
+			<Text text={text} />
+		</svg>
+	)
+}
+
+export default Pie
