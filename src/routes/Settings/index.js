d9a762004c2ed3d1fbcb83380fbe9e80606500fb
tharunoptimus <tharunoptimus@outlook.com>
2022-10-30 15:57:13 +0530

    Added `Settings` component with its assets

--- src/routes/Settings/index.js
+++ src/routes/Settings/index.js
@@ -1,12 +1,67 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import Title from "../../components/Title"
+import styles from "./Settings.module.css"
+import { useState, useRef, useEffect } from "react"
+import { get, set } from "idb-keyval"
+
 
 export default function Settings() {
 
+
+	
+	let navigate = useNavigate()
 	let { accountId } = useParams()
+	
+	let [issuer, setIssuer] = useState("")
+	let issuerInput = useRef()
+	
+	let onInputKeyPress = () => {
+		setIssuer(issuerInput.current.value)
+	}
+
+	let saveToDatabase = () => {
+		async function run() {
+			let accounts = await get("accounts")
+			
+			accounts.forEach((account) => {
+				if (account.id === accountId) {
+					account.issuer = issuer.trim()
+				}
+			})
+			set("accounts", accounts)
+		}
+		run()
+		navigate(`/account/${accountId}?update=true`)
+	}
+	
+	useEffect(() => {
+		let returnHome = () => navigate("/")
+		async function retriveData() {
+			let accounts = await get("accounts")
+			let account = accounts.filter((account) => account.id === accountId)[0]
+			if (account != null) return account
+			returnHome()
+		}
+		async function run() {
+			let account = await retriveData(accountId)
+			if (account != null) {
+				setIssuer(account.issuer)
+			}
+		}
+		run()
+	}, [accountId, navigate])
 
 	return (
 		<>
-			<span>This is the Settings Page for Account:  {accountId}</span>
+			<Title titleName="Settings" />
+			<div className={styles.container}>
+				<div className={styles.settingsCard}>
+					<span className={styles.infoSpan}>Edit Issuer Name</span>
+					<input ref={issuerInput} onChange={onInputKeyPress} type="text" value={issuer} placeholder="Issuer" />
+					<button onClick={saveToDatabase} className={styles.button}>Save Changes</button>
+				</div>
+			</div>
+			
 		</>
 	)
 }
