30e0b12c3d7d351be9fe92513d222d6679c31685
tharunoptimus <tharunoptimus@outlook.com>
2022-11-02 19:59:00 +0530

    Modified `Title` for Menu Actions

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,29 +1,33 @@
 import left from "./left.svg"
 import styles from "./Title.module.css"
 
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 function Title(props) {
+	let navigate = useNavigate()
 
-    let navigate = useNavigate()
+	let { titleName, back } = props
 
-    let handleClick = () => {
-        setTimeout(() => {
-            navigate("/")
-        }, 100)
-    }
+	let handleClick = () => {
+		setTimeout(() => {
+			if (back) {
+				return navigate(back)
+			}
+			navigate("/")
+		}, 100)
+	}
 
-    return (
-        <>
-            <div className={styles.header}>
-                <span className={styles.actionIconsAdd} onClick={handleClick}>
-                    <img src={left} alt="Go Back" />
-                </span>
-                
-                <span className={styles.appName}>{props.titleName}</span>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className={styles.header}>
+				<span className={styles.actionIconsAdd} onClick={handleClick}>
+					<img src={left} alt="Go Back" />
+				</span>
+
+				<span className={styles.appName}>{titleName}</span>
+			</div>
+		</>
+	)
 }
 
 export default Title
