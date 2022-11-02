e6811d5a2cb9250005612b8c7a91fd9f8f6c6de9
tharunoptimus <tharunoptimus@outlook.com>
2022-11-03 01:00:23 +0530

    Added UI

--- src/routes/Import/index.js
+++ src/routes/Import/index.js
@@ -1,7 +1,27 @@
+import Title from "../../components/Title"
+import styles from "./Import.module.css"
+import fileDecryptImage from "./decrypt.webp"
+
 export default function Import() {
 	return (
 		<>
+			<Title titleName="Import" back={"/menu"} />
+
+			<div className={styles.import}>
+				<span className={styles.infoSpan}>
+					Import your accounts from a filelock backup file
+				</span>
+
+				<img
+					src={fileDecryptImage}
+					alt="Decryption"
+					className={styles.encrypt}
+					width={100}
+					height={100}
+				/>
+
 			<span>This is the Import page</span>
+			</div>
 		</>
 	)
 }
