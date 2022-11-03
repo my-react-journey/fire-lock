6fb860b6e3e8d04b99df86e2ba6ae9bac2e0decb
tharunoptimus <tharunoptimus@outlook.com>
2022-11-04 02:33:38 +0530

    I don't want prototyping folder anymore.

--- prototyping/speak/index.js
+++
@@ -1,9 +0,0 @@
-let speakeasy = require("speakeasy")
-let fs = require("fs")
-let secret = speakeasy.generateSecret({length: 17})
-
-// write to file with name secret.txt
-fs.writeFile('secret.txt', secret.base32, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-});
